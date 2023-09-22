fd5669ffaf5011d7d5c3ac89f8ade21c646f4259
Amankr <109649639+amankr794@users.noreply.github.com>
2023-09-22 12:02:05 +0530

    write first c++ program

---
+++ day 3 (write first c++ program)/first.cpp
@@ -0,0 +1,68 @@
+/* Day 3/180 */
+
+// write first cpp program
+
+#include<iostream>
+using namespace std;
+int main(){
+    
+    /* How to print on console screen */
+
+    // std::cout<<"Hello Coder Army";
+    // std::cout<<"Hello Coder Army";
+    // std::cout<<"Hello Coder Army";
+    // std::cout<<"Hello Coder Army";
+    // std::cout<<"Hello Coder Army";
+
+    // cout<<"Hello Coder Army";
+    // cout<<"Hello Coder Army   ";
+
+    // to print in next line
+    // cout<<"Hello Coder Army"<<endl;
+    // cout<<"Hello Coder Army\n";
+
+    // cout<<3<<endl;
+    // cout<<"3"<<endl;
+    // cout<<3+2<<" "<<"3+2"<<endl;
+    // cout<<3*2<<" "<<"3*2"<<endl;
+
+
+    /* how to use variable and datatype in code */
+
+    //int type variable
+
+    // int a; // declaring and defining a variable
+    // a=5; // initializing a variable
+    // cout<<a<<" "; //printing the value of variable on console
+    // cout<<"a"<<" ";
+    // cout<<'a'<<"\n"<<'\n';
+
+    // int b=10; // declaring+defining+initializing a variable
+
+    //declaring and defining more than one variable in asingle line
+    // int c=2, d=8, e=78, f=-45;
+
+    //sum of two variable
+    // cout<<c+d;
+
+
+    //char type variable
+    // char c='a';
+    // cout<<c<<endl;
+
+    //float variable
+    // float f=3.2;
+    // cout<<f<<endl;
+
+    //double variable
+    // double d=56.89;
+    // cout<<d<<endl;
+
+    //boolean variable
+    bool a=true;cout<<a<<endl;
+    bool b=false;cout<<b<<endl;
+    bool c=23;cout<<c<<endl;
+    bool d=0;cout<<d<<endl;
+    bool e=0.0001;cout<<e;
+
+}
