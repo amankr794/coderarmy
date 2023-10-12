c9c795513416d1bffa1014c2fa2c7acb8b351bbc
Amankr <109649639+amankr794@users.noreply.github.com>
2023-10-12 23:55:15 +0530

    sheet solutions

---
+++ day 17(leetcode questions)/sheet solutions.cpp
@@ -0,0 +1,23 @@
+/** 1> Add Digits - https://leetcode.com/problems/add-digits/description/ **/
+
+/** 
+  2> Leap Year - https://practice.geeksforgeeks.org/problems/leap-year0943/1?utm_source=geeksforgeeks&utm_medium=ml_article_practice_tab&utm_campaign=article_practice_tab
+  
+**/
+
+/** 3> Reverse Integer: https://leetcode.com/problems/reverse-integer/description/ **/
+
+/** 4> Sqrt(x): https://leetcode.com/problems/sqrtx/description/ **/
+
+/** 5> Palindrome Number: https://leetcode.com/problems/palindrome-number/description/ **/
+
+/** 6> Complement of Base 10 Integer: https://leetcode.com/problems/complement-of-base-10-integer/description/ **/
+
+/** 7> Power of Two: https://leetcode.com/problems/power-of-two/description/ **/
+
+/** 8> Ugly Number: https://leetcode.com/problems/ugly-number/description/ **/
+
+/** 
+    9> Squares in N*N Chessboard:
+    https://practice.geeksforgeeks.org/problems/squares-in-nn-chessboard1801/1?page=1&difficulty[]=-1&difficulty[]=0&category[]=Numbers&category[]=number-theory&sortBy=submissions
+**/
