fb6b59c731c4f9546023e2f1c1dac8891adbe415
Amankr <109649639+amankr794@users.noreply.github.com>
2023-10-24 22:17:08 +0530

    sheetSolutions

--- Day 30 (Binary Search)/sheetSolutions.cpp
+++ Day 30 (Binary Search)/sheetSolutions.cpp
@@ -62,3 +62,97 @@ int binarySearch(int arr[], int n, int key){
     // executes if key is not present in the array;
     return -1;
 }
+
+
+
+/***
+    Q2> Search Insert Position
+
+    Leetcode Link : https://leetcode.com/problems/search-insert-position/
+
+    Gfg link : https://practice.geeksforgeeks.org/problems/search-insert-position-of-k-in-a-sorted-array/1
+
+***/
+
+/** Method - 1 **/
+int searchInsert(vector<int> &Arr, int k)
+{
+
+    int N = Arr.size();
+    int start = 0, end = N - 1, mid;
+
+    while (start <= end)
+    {
+
+        // find mid
+        mid = start + (end - start) / 2;
+
+        // arr[mid]==key
+        if (Arr[mid] == k)
+            return mid;
+
+        // arr[mid]<key
+        else if (Arr[mid] < k)
+            start = mid + 1;
+
+        // arr[mid]>key
+        else
+            end = mid - 1;
+    }
+
+    // comes here means key is not found in array
+
+    // key will present at start index  of array
+    return start;
+
+    // Note : return : the start if you don't find the target element in the array
+
+}
+
+
+/** Method - 2 **/
+int searchInsertK(vector<int> Arr, int N, int k)
+{
+    // code here
+    // 2nd approach
+
+    int start = 0, end = N - 1, mid;
+
+    while (start <= end)
+    {
+
+        // find mid
+        mid = start + (end - start) / 2;
+
+        // arr[mid]==key
+        if (Arr[mid] == k)
+            return mid;
+
+        // arr[mid]>key
+        else if (Arr[mid] > k)
+        {
+
+            if (mid == 0) // corner case : All the elements in array are greater than target element
+                return 0;
+
+            else if (Arr[mid - 1] < k)
+                return mid;
+            else
+                end = mid - 1;
+        }
+
+        // arr[mid]<key
+        else if (Arr[mid] < k)
+        {
+
+            if (mid == N - 1) // corner case : All the elements in array are smaller than target element
+                return N;
+
+            else
+                start = mid + 1;
+        }
+    }
+
+    // never executes only used to avoid error
+    return 0;
+}
