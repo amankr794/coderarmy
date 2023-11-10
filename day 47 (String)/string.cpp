ff97b9ec302d7c637a8b9ca26cdbcac5a368a551
Amankr <109649639+amankr794@users.noreply.github.com>
2023-11-10 14:25:00 +0530

    string

---
+++ day 47 (String)/string.cpp
@@ -0,0 +1,79 @@
+/*** String in C++ ***/
+
+#include<iostream>
+using namespace std;
+int main(){
+    
+    /** Declaring and initializing a string ***/
+
+    // string s = "Aman";
+    // cout<<s;
+
+    // cout<<s[0];
+    // cout<<s[1];
+    // cout<<s[2];
+    // cout<<s[3];
+    // cout<<s[4];
+
+    // string s = "Aman Kumar";
+    // cout<<s;
+
+    /** Taking string as input from user **/
+    // string s ;
+    // cin>>s;
+    // cout<<s;
+    
+    /** Reading multiword string **/
+    // string s;
+    // getline(cin,s);
+    // cout<<s<<endl;
+
+    /*size of string*/
+    // cout<<s.size();
+
+    /** Concatenate/append two strings **/
+    // string s1 = "rohit", s2 = "mohit";
+    
+    /* 1st method of append (using + sign) */
+    // string s3 = s1+s2;
+    // cout<<s3;
+
+    /* 2nd method of append using append() function */
+    // string s3 = s1.append(s2);
+    // cout<<s3;
+
+
+    /** Adding a charater at the end of string **/
+    // string s1 = "Aman";
+    
+    // s1.push_back('p');
+    // cout<<s1;
+
+    /* or */
+    // s1 = s1 + "p";
+    // cout<<s1;
+
+    /* or */
+    // s1 = s1 + 'p';
+    // cout<<s1;
+
+
+    /** Removing a charater from the end of string **/
+    // string s1 = "Aman";
+    // s1.pop_back();
+    // cout<<s1;
+
+    /** Use of escape sequence **/
+    // string s1 = "Aman is a \"good\" boy ";
+    // cout<<s1;
+
+    /* printing a null character */
+    // string s1 = "\\0";
+    // cout<<s1;
+
+    // string s1 = "\0";    // Home work
+    // cout<<s1;
+
+
+
+}
